a5f760ac73303c3d240f5f234cc1e09be5953c09
Tshilidzi-Given-Maphiri <104319025+Tshilidzi-Given-Maphiri@users.noreply.github.com>
2023-04-25 19:04:01 +0200

    Update Program.cs
    added a set ingredients method, that takes in the ingredient name, quantity and unit of measurement.

--- ST10086197_PROG2A_POE_PART1_Given/Program.cs
+++ ST10086197_PROG2A_POE_PART1_Given/Program.cs
@@ -18,6 +18,26 @@ public class Recipe
         numSteps = 0;
     }
 
+    public void SetIngredients(int numIngredients)
+    {
+        this.numIngredients = numIngredients;
+        ingredientNames = new string[numIngredients];
+        ingredientQuantities = new double[numIngredients];
+        ingredientUnits = new string[numIngredients];
+
+        for (int i = 0; i < numIngredients; i++)
+        {
+            Console.WriteLine("Enter ingredient name:");
+            ingredientNames[i] = Console.ReadLine();
+
+            Console.WriteLine("Enter ingredient quantity:");
+            ingredientQuantities[i] = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter ingredient unit of measurement:");
+            ingredientUnits[i] = Console.ReadLine();
+        }
+    }
+
     public void ClearRecipe()
     {
         numIngredients = 0;
@@ -28,7 +48,11 @@ public class Recipe
 
     static void Main(String[] args)
     {
+        Recipe recipe = new Recipe();
 
+        Console.WriteLine("Enter the number of ingredients:");
+        int numIngredients = int.Parse(Console.ReadLine());
+        recipe.SetIngredients(numIngredients);
     }
 
 

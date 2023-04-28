66b15bb06b6e0cab65360f415487d3a89b816547
Tshilidzi-Given-Maphiri <104319025+Tshilidzi-Given-Maphiri@users.noreply.github.com>
2023-04-28 20:19:52 +0200

    edited readme
    added link to GitHub to readme file

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ST10086197_PROG2A_POE_PART1_Given
+
+
+Module Code: PROG6221 Title: PROG6221 POE Part 1
+in order to run the application:
+1. access the .cs files under the St10086197_Prog2a_poe_Part1_Given folder.
+2. download the entire folder or open the files to look at specific code.
+3. once downloaded run program
+4. follow the steps given in the console application.
+
+
+Link to GitHub:
+https://github.com/Tshilidzi-Given-Maphiri/ST10086197_PROG2A_POE_PART1_Given.git
+
--- ST10086197_PROG2A_POE_PART1_Given/Program.cs
+++ ST10086197_PROG2A_POE_PART1_Given/Program.cs
@@ -336,60 +336,7 @@ public class Recipe
         }
     }
 
-    static void Main(String[] args)
-    {
-        Recipe recipe = new Recipe();
-
-        Console.ForegroundColor = ConsoleColor.Blue;
-        Console.WriteLine("-------RECIPE_APP-------");
-        Console.ForegroundColor = ConsoleColor.White;
-
-        
-
-        
-
-        Console.ForegroundColor = ConsoleColor.Blue;
-        Console.WriteLine("----------Recipe:---------------");
-        Console.ForegroundColor = ConsoleColor.White;
-        Console.WriteLine("Enter the number of ingredients:");
-        int numIngredients = int.Parse(Console.ReadLine());
-        recipe.SetRecipe(numIngredients);
-
-        Console.WriteLine("");
-
-        Console.WriteLine("Enter the number of steps:");
-        int numSteps = int.Parse(Console.ReadLine());
-        recipe.SetSteps(numSteps);
-
-        
-
-         Console.ForegroundColor = ConsoleColor.Blue;
-         Console.WriteLine("----------Recipe:---------------");
-         Console.ForegroundColor = ConsoleColor.White;
-         recipe.DisplayRecipe();
-
-
-         Console.WriteLine("");
-
-         Console.WriteLine("-------------------------------------------");
-
-         
-
-        Console.WriteLine("Enter scale factor (0.5 (half), 1 (original), 2 (double), or 3 (triple)):");
-        double scaleFactor = double.Parse(Console.ReadLine());
-        recipe.ScaleRecipe(scaleFactor);
-        
-
-        Console.ForegroundColor = ConsoleColor.Blue;
-        Console.WriteLine($"Recipe scaled by a factor of {scaleFactor}:");
-        Console.ForegroundColor = ConsoleColor.White;
-        recipe.DisplayRecipe();
-
-
-
-        recipe.begin();
-
-    }
+   
 
     
 }

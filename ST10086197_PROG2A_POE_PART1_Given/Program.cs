f054746ac27156411afa423983c27c233365a420
Tshilidzi-Given-Maphiri <104319025+Tshilidzi-Given-Maphiri@users.noreply.github.com>
2023-06-06 15:13:40 +0200

    input recipe method
    added a new method that contains all of the necessary tasks involved in creating a user inputting the values for a new recipe.

--- ST10086197_PROG2A_POE_PART1_Given/Program.cs
+++ ST10086197_PROG2A_POE_PART1_Given/Program.cs
@@ -1,38 +1,192 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
 
 namespace ST10086197_PROG2A_POE_PART1_Given
 {
     public class Program
     {
+
+        
+
+        public static void inputRecipe(SortedList<string, Recipe> recipes)
+        {
+
+            // Get recipe input from the user.
+            
+                Console.WriteLine("Enter the name of the recipe: ");
+                string name = Console.ReadLine();
+                
+                Console.WriteLine("Enter the number of ingredients: ");
+                int numberOfIngredients = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < numberOfIngredients; i++)
+                {
+                    Console.WriteLine("Ingredient name: ");
+                    string ingredientName = Console.ReadLine();
+                    Console.WriteLine("Ingredient quantity: ");
+                    int ingredientQuantity = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Ingredient unit of measurement: ");
+                    string ingredientUnitOfMeasurement = Console.ReadLine();
+                    Console.WriteLine("Ingredient calories: ");
+                    int ingredientCalories = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Ingredient food group: ");
+                    string ingredientFoodGroup = Console.ReadLine();
+                    if (recipes.ContainsKey(name))
+                    {
+                        recipes[name].ingredients.Add(
+                            new Ingredient
+                            {
+                                ingrName = ingredientName,
+                                ingrQuantity = ingredientQuantity,
+                                ingrUnitOfMeasurement = ingredientUnitOfMeasurement,
+                                Calories = ingredientCalories,
+                                FoodGroup = ingredientFoodGroup
+                            });
+                    }
+                    else
+                    {
+                        Recipe recipe = new Recipe()
+                        {
+                            recipeName = name,
+                            ingredients = new List<Ingredient>()
+                            {
+                                new Ingredient
+                                {
+                                    ingrName = ingredientName,
+                                    ingrQuantity = ingredientQuantity,
+                                    ingrUnitOfMeasurement = ingredientUnitOfMeasurement,
+                                    Calories = ingredientCalories,
+                                    FoodGroup = ingredientFoodGroup
+                                }
+                            }
+                        };
+                        recipes.Add(name, recipe);
+                    }
+                }
+            
+
+            /*   // Gets the user to input a recipe
+
+               // prompt for user input
+               Console.WriteLine("Enter the name of the recipe: ");
+               string name = Console.ReadLine();
+
+
+               //enter ingredients
+               Console.WriteLine("Enter the number of ingredients: ");
+               int numberOfIngredients = Convert.ToInt32(Console.ReadLine());
+
+               for (int i = 0; i < numberOfIngredients; i++)
+               {
+                   Console.WriteLine("Ingredient name: ");
+                   string ingredientName = Console.ReadLine();
+
+                   Console.WriteLine("Ingredient quantity: ");
+                   int ingredientQuantity = Convert.ToInt32(Console.ReadLine());
+
+                   Console.WriteLine("Ingredient unit of measurement: ");
+                   string ingredientUnitOfMeasurement = Console.ReadLine();
+
+                   Console.WriteLine("Ingredient calories: ");
+                   int ingredientCalories = Convert.ToInt32(Console.ReadLine());
+
+                   Console.WriteLine("Ingredient food group: ");
+                   string ingredientFoodGroup = Console.ReadLine();
+
+
+
+                   recipes.Add(name, new Recipe()
+                   {
+                       recipeName = name,
+                       ingredients = new List<Ingredient>()
+                       {
+                           new Ingredient
+                           {
+                               ingrName = ingredientName,
+                               ingrQuantity = ingredientQuantity,
+                               ingrUnitOfMeasurement = ingredientUnitOfMeasurement,
+                               Calories = ingredientCalories,
+                               FoodGroup = ingredientFoodGroup
+                           }
+                       }
+
+
+                   });
+                   //recipe.addIngredients(ingredientName, ingredientQuantity, ingredientUnitOfMeasurement);  
+               }
+               foreach (var kvp in recipes)
+               {
+                   Console.WriteLine("Key: {0}, value: {1}", kvp.Key, kvp.Value);
+               }
+            */
+        }
+
+
         static void Main(String[] args)
         {
-            // creates a new recipe 
-            Recipe recipe = new Recipe();
 
- // display informational text
+            // Create a sorted list of recipes.
+            SortedList<string, Recipe> recipes = new SortedList<string, Recipe>();
+
+            // creates a new recipe 
+            // Recipe recipe = new Recipe();
+
+            // display informational text
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("-------RECIPE_APP-------");
             Console.ForegroundColor = ConsoleColor.White;
 
 
-            // prompt for user input
-            Console.WriteLine("Enter the name of the recipe: ");
-            string name = Console.ReadLine();
+            inputRecipe(recipes);
 
-            //enter ingredients
-            Console.WriteLine("Enter the number of ingredients: ");
-            int numberOfIngredients = Convert.ToInt32(Console.ReadLine());
+            
+            
+            /*  // prompt for user input
+              Console.WriteLine("Enter the name of the recipe: ");
+              string name = Console.ReadLine();
 
-            for (int i = 0; i < numberOfIngredients; i++)
-            {
-                Console.WriteLine("Ingredient name: ");
-                string ingredientName = Console.ReadLine();
-                Console.WriteLine("Ingredient quantity: ");
-                int ingredientQuantity = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Ingredient unit of measurement: ");
-                string ingredientUnitOfMeasurement = Console.ReadLine();
-                recipe.addIngredients(ingredientName, ingredientQuantity, ingredientUnitOfMeasurement);
-            }
+              //enter ingredients
+              Console.WriteLine("Enter the number of ingredients: ");
+              int numberOfIngredients = Convert.ToInt32(Console.ReadLine());
+
+              for (int i = 0; i < numberOfIngredients; i++)
+              {
+                  Console.WriteLine("Ingredient name: ");
+                  string ingredientName = Console.ReadLine();
+
+                  Console.WriteLine("Ingredient quantity: ");
+                  int ingredientQuantity = Convert.ToInt32(Console.ReadLine());
+
+                  Console.WriteLine("Ingredient unit of measurement: ");
+                  string ingredientUnitOfMeasurement = Console.ReadLine();
+
+                  Console.WriteLine("Ingredient calories: ");
+                  int ingredientCalories = Convert.ToInt32(Console.ReadLine());
+
+                  Console.WriteLine("Ingredient food group: ");
+                  string ingredientFoodGroup = Console.ReadLine();
+
+
+                  recipes.Add(name, new Recipe()
+                  {
+                      recipeName = name,
+                      ingredients = new List<Ingredient>()
+                      {
+                          new Ingredient
+                          {
+                              ingrName = ingredientName,
+                              ingrQuantity = ingredientQuantity,
+                              ingrUnitOfMeasurement = ingredientUnitOfMeasurement,
+                              Calories = ingredientCalories,
+                              FoodGroup = ingredientFoodGroup
+                          }
+                      }
+                  });
+                  //recipe.addIngredients(ingredientName, ingredientQuantity, ingredientUnitOfMeasurement);
+              }
+
+              */
 
             // enter steps/instructions
 
@@ -42,7 +196,7 @@ namespace ST10086197_PROG2A_POE_PART1_Given
             {
                 Console.WriteLine("Step " + (i + 1) + ": ");
                 string stepInstruction = Console.ReadLine();
-                recipe.SetSteps(stepInstruction);
+                //recipe.SetSteps(stepInstruction);
             }
 
 
@@ -75,7 +229,7 @@ namespace ST10086197_PROG2A_POE_PART1_Given
 
 
     //Display the recipe
-            recipe.DisplayRecipe();
+           // recipe.DisplayRecipe();
 
 
             Console.WriteLine("");
@@ -91,8 +245,8 @@ namespace ST10086197_PROG2A_POE_PART1_Given
             {
                 Console.WriteLine("Enter the scale factor (0.5 (half), 2 (double), or 3 (triple)): ");
                 double scaleFactor = Convert.ToDouble(Console.ReadLine());
-                recipe.ScaleRecipe();
-                recipe.DisplayRecipe();
+                //recipe.ScaleRecipe();
+                //recipe.DisplayRecipe();
             }
 
 
@@ -101,8 +255,8 @@ namespace ST10086197_PROG2A_POE_PART1_Given
             response = Console.ReadLine();
             if (response == "Y")
             {
-                recipe.ResetQuantities();
-                recipe.DisplayRecipe();
+                //recipe.ResetQuantities();
+                //recipe.DisplayRecipe();
             }
             /* Console.WriteLine("Enter scale factor (0.5 (half), 1 (original), 2 (double), or 3 (triple)):");
              double scaleFactor = double.Parse(Console.ReadLine());
@@ -121,6 +275,9 @@ namespace ST10086197_PROG2A_POE_PART1_Given
              */
 
         }
+
+
+        
     }
    
 
